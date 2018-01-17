f8973b65f4617524924e9a6c76568a5b53b8ec36
KarasawaTakahiro <karasawa@basicinc.jp>
2018-01-17 12:14:36 +0900

    create

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <Datetime v-model="datetime" type="datetime" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import 'vue-datetime/dist/vue-datetime.css'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/components/Datetime.vue
@@ -0,0 +1,19 @@
+<template>
+  <Datetime/>
+</template>
+
+<script>
+import { Datetime } from 'vue-datetime'
+import 'vue-datetime/dist/vue-datetime.css'
+
+Vue.extend({
+  template: 'Datetime',
+  components: {
+    datetime: Datetime
+  }
+})
+
+</script>
+
+<style scoped>
+</style>
